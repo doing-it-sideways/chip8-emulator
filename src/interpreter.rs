f996e3a8f2324efcfab38b095e5f0fcbb55f5bdd
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-03-07 21:17:37 -0500

    defined graphics array

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -17,7 +17,12 @@ impl From<u16> for Address {
 
 struct Registers {
     v: [u8; 0x10], // 16 general purpose register V0-VF, VF often modified by instructions as a flag register
-    i: Address,
+    i: Address, // program counter
+}
+
+struct Chip8 {
+    reg: Registers,
+    pixels: [u64; 0x20] // 64x32 on/off values
 }
 
 pub fn run(rom_data: Vec<u8>) -> Result<(), Box<dyn Error>> {
