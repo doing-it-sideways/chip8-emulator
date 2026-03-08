51c300a34738f3fe41b2abe8cdf8ba1d9d418a3e
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-03-07 21:40:33 -0500

    instruction tuple struct

--- src/interpreter/instrs.rs
+++ src/interpreter/instrs.rs
@@ -1,5 +1,17 @@
 use super::{ Address, font };
 
+pub struct Instruction(u16);
+
+impl Instruction {
+    pub fn X(&self) -> u8 {
+        ((self.0 & 0x0F00) >> 8) as u8
+    }
+
+    pub fn Y(&self) -> u8 {
+        ((self.0 & 0x00F0) >> 4) as u8
+    }
+}
+
 pub enum Instructions {
     
 }
