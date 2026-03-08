89bed0dcef7dd6a62b57aab9d13fa85ebb9471d5
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-03-07 20:46:03 -0500

    address impl

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,19 +1,19 @@
-use std::error::Error;
+use std::{
+    error::Error,
+};
 
-#[derive(Debug)]
-pub enum InterpreterErr {
+mod error;
+mod instrs;
+mod font;
 
-}
+#[derive(PartialEq, PartialOrd)]
+struct Address(u16);
 
-impl std::fmt::Display for InterpreterErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!();
+impl From<u16> for Address {
+    fn from(addr: u16) -> Self {
+        Address(addr & 0x0FFF)
     }
 }
-
-impl Error for InterpreterErr {}
-
-mod font;
 
 pub fn run(rom_data: Vec<u8>) -> Result<(), Box<dyn Error>> {
     Ok(())
