b372d94426aaef749da4d12b3f2c8fa88ffec169
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-03-07 21:47:58 -0500

    conform with standards

--- src/interpreter/instrs.rs
+++ src/interpreter/instrs.rs
@@ -3,11 +3,11 @@ use super::{ Address, font };
 pub struct Instruction(u16);
 
 impl Instruction {
-    pub fn X(&self) -> u8 {
+    pub fn x(&self) -> u8 {
         ((self.0 & 0x0F00) >> 8) as u8
     }
 
-    pub fn Y(&self) -> u8 {
+    pub fn y(&self) -> u8 {
         ((self.0 & 0x00F0) >> 4) as u8
     }
 }
