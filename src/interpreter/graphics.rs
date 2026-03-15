3ce58511a8fd4ea6cf25a3536ce994235df3f868
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-03-15 00:17:06 -0400

    graphics segmented into init and draw

--- src/interpreter/graphics.rs
+++ src/interpreter/graphics.rs
@@ -4,49 +4,67 @@ use std::{
 };
 
 use sdl3::{ 
-    pixels::Color,
     event::Event,
     keyboard::Keycode,
+    pixels::Color,
 };
 
 const WIDTH: u32 = 64;
 const HEIGHT: u32 = 32;
 
-pub fn init(window_scale: u8) -> Result<(), Box<dyn Error>> {
-    let ctx = sdl3::init()?;
-    let vid_subsys = ctx.video()?;
+struct GraphicsCtx {
+    ctx: sdl3::Sdl,
+    canvas: sdl3::render::WindowCanvas,
+    event_pump: sdl3::EventPump,
+}
 
-    let window = vid_subsys.window("Chip-8 Interpreter", WIDTH * window_scale as u32, HEIGHT * window_scale as u32)
-        .opengl()
-        .position_centered()
-        .build()?;
+struct QuitEvent;
 
-    let mut canvas  = window.into_canvas();
+impl GraphicsCtx {
+    pub fn init(window_scale: u8) -> Result<Self, Box<dyn Error>> {
+        let ctx = sdl3::init()?;
+        let vid_subsys = ctx.video()?;
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+        let window = vid_subsys.window("Chip-8 Interpreter",
+                                        WIDTH * window_scale as u32,
+                                        HEIGHT * window_scale as u32)
+            .opengl()
+            .position_centered()
+            .build()?;
 
-    let mut event_pump = ctx.event_pump()?;
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        let mut canvas  = window.into_canvas();
+
+        canvas.set_draw_color(Color::RGB(0, 255, 255));
         canvas.clear();
+        canvas.present();
 
-        for event in event_pump.poll_iter() {
+        let mut event_pump = ctx.event_pump()?;
+    
+        Ok(GraphicsCtx {
+            ctx,
+            canvas,
+            event_pump
+        })
+    }
+
+    pub fn draw(&mut self) -> Result<(), QuitEvent> {
+        let mut i = 0;
+        i = (i + 1) % 255;
+        self.canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        self.canvas.clear();
+
+        for event in self.event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
+                    return Err(QuitEvent);
                 },
                 _ => {}
             }
         }
 
-        canvas.present();
+        self.canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    }
-    
-    Ok(())
-}
 
+        Ok(())
+    }
+}
