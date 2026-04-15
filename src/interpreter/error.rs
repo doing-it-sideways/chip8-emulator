95b6bcaf5e0ff7e4b670d4c1d996e1dadb11f795
doing-it-sideways <90480275+doing-it-sideways@users.noreply.github.com>
2026-04-15 01:45:58 -0400

    expanded error a teensy bit

--- src/interpreter/error.rs
+++ src/interpreter/error.rs
@@ -9,7 +9,10 @@ pub enum InterpreterErr {
 
 impl std::fmt::Display for InterpreterErr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Chip-8 interpreter error: TODO")
+        write!(f, "Chip-8 interpreter error: {}", match *self {
+            Self::InvalidInstr => "Invalid instruction",
+            _ => "Unhandled error"
+        })
     }
 }
 
